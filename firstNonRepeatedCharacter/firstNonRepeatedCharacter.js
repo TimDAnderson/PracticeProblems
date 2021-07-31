7374ca54d349e2cb0a2626cb13299fb619978185
Tim Anderson <tdanderson805@gmail.com>
2021-07-30 17:52:34 -0700

    firstNonRepeatedCharacter

---
+++ firstNonRepeatedCharacter/firstNonRepeatedCharacter.js
@@ -0,0 +1,31 @@
+/**
+ * Given an arbitrary input string, return the first nonrepeated character in
+ * the string. For example:
+ *
+ *   firstNonRepeatedCharacter('ABA'); // => 'B'
+ *   firstNonRepeatedCharacter('AACBDB'); // => 'C'
+ */
+
+ var firstNonRepeatedCharacter = function(string) {
+  // TODO: your solution here
+  var returnObj = {};
+ 
+  //make an object we push to
+  for (var i = 0; i < string.length; i++) {
+    if (returnObj[string[i]] === undefined) {
+      returnObj[string[i]] = 1;
+    } else {
+      returnObj[string[i]] =  returnObj[string[i]] + 1;
+    }
+  }
+
+  for (var i = 0; i < string.length; i++) {
+    if (returnObj[string[i]] === 1) {
+      return string[i];
+    }
+  }
+
+};
+
+console.log(firstNonRepeatedCharacter('ABA'));
+console.log(firstNonRepeatedCharacter('AACBDB'));
