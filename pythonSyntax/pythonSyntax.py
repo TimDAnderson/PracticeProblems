378cf7effb7615c2ed3ff93039a5c82408a72b8b
Tim Anderson <tdanderson805@gmail.com>
2021-08-12 16:00:03 -0700

    python syntax

---
+++ pythonSyntax/pythonSyntax.py
@@ -0,0 +1,12 @@
+def helloWorld():
+  print("hi")
+
+helloWorld()
+
+def returnLargestInt(arr):
+  print(len(arr))
+  print(arr[0])
+  for item in arr:
+    print(item)
+
+returnLargestInt([1, 2, 3, 4, 5])
